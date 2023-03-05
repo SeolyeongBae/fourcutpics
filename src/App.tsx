2755c72a3b99a386d57939e0e60ad9dc80c8ef2f
seolyeongbae <peixueying@gmail.com>
2023-03-05 12:07:41 +0900

    refactor: code clenaning

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,16 @@ import Webcam from "react-webcam";
 import "./App.css";
 
 function App() {
-  const videoRef = useRef<HTMLVideoElement>(null);
-
   const [picNums, setPicNums] = useState(0);
   const [pics, setPics] = useState([]);
 
   const webcamRef = useRef<any>(null);
-  const [imgSrc, setImgSrc] = useState(null);
 
   const capture = useCallback(() => {
     const imageSrc = webcamRef.current!.getScreenshot();
-    setImgSrc(imageSrc);
     setPics((prev) => prev.concat(imageSrc));
     setPicNums((prev) => prev + 1);
-  }, [webcamRef, setImgSrc]);
+  }, [webcamRef]);
 
   useEffect(() => {
     if (picNums > 3) {
@@ -39,6 +35,7 @@ function App() {
   function GoToCamera() {
     // 다시 촬영
     setPicNums(() => 0);
+    setPics(() => []);
   }
 
   return (
@@ -51,69 +48,100 @@ function App() {
       }}
     >
       <>
-        <Webcam
-          audio={false}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          style={{
-            width: "1024px",
-            height: "768px",
-          }}
-        />
-
         {picNums <= 3 ? (
-          <div
-            onClick={capture}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "70px",
-              height: "70px",
-              margin: "10px",
-              borderRadius: "100px",
-              position: "absolute",
-              zIndex: "101",
-              bottom: "5%",
-              left: "46%",
-              cursor: "pointer",
-              backgroundColor: "white",
-            }}
-          >
+          <>
+            <Webcam
+              audio={false}
+              ref={webcamRef}
+              screenshotFormat="image/jpeg"
+              style={{
+                width: "1024px",
+                height: "768px",
+              }}
+            />
+            <div
+              onClick={capture}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "70px",
+                height: "70px",
+                margin: "10px",
+                borderRadius: "100px",
+                position: "absolute",
+                zIndex: "101",
+                bottom: "5%",
+                left: "46%",
+                cursor: "pointer",
+                backgroundColor: "white",
+              }}
+            >
+              <div
+                style={{
+                  textAlign: "center",
+                  width: "60px",
+                  height: "60px",
+                  border: "2px solid",
+                  borderRadius: "100px",
+                }}
+              ></div>
+            </div>
+          </>
+        ) : (
+          <>
             <div
               style={{
-                textAlign: "center",
-                width: "60px",
-                height: "60px",
-                border: "2px solid",
-                borderRadius: "100px",
+                width: "1920px",
+                height: "1080px",
+                display: "grid",
+                gridTemplateColumns: "512px 512px",
+                gridRow: "auto auto",
+                gridColumnGap: "20px",
+                gridRowGap: "20px",
+                margin: "20px auto",
+                backgroundColor: "#fff",
               }}
-            ></div>
-          </div>
-        ) : (
-          <div
-            onClick={GoToCamera}
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              alignItems: "center",
-              width: "70px",
-              height: "70px",
-              margin: "10px",
-              borderRadius: "10px",
-              position: "absolute",
-              zIndex: "101",
-              bottom: "5%",
-              left: "46%",
-              cursor: "pointer",
-              backgroundColor: "white",
-            }}
-          >
-            <p>다시 촬영</p>
-          </div>
+            >
+              {pics.length !== 0 &&
+                pics.map((pic) => (
+                  <img
+                    src={pic}
+                    style={{
+                      padding: "20px",
+                      display: "flex",
+                      alignItems: "center",
+                      justifyContent: "center",
+                      width: "512px",
+                      height: "384px",
+                    }}
+                  />
+                ))}
+            </div>
+
+            <div
+              onClick={GoToCamera}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "70px",
+                height: "70px",
+                margin: "10px",
+                borderRadius: "10px",
+                position: "absolute",
+                zIndex: "101",
+                bottom: "5%",
+                left: "46%",
+                cursor: "pointer",
+                backgroundColor: "white",
+              }}
+            >
+              <p>다시 촬영</p>
+            </div>
+          </>
         )}
       </>
-      {imgSrc && <img src={imgSrc} />}
     </div>
   );
 }
