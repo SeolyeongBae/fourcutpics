e4f5a69b4b31451c7c75da6d0097003d65d16bc1
seolyeongbae <peixueying@gmail.com>
2023-03-05 12:32:33 +0900

    feat: implement download

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,14 @@ import "./App.css";
 function App() {
   const [picNums, setPicNums] = useState(0);
   const [pics, setPics] = useState([]);
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  const [canvasState, setCanvasState] = useState("none");
+  const canvasLocation = [
+    [50, 100],
+    [582, 100],
+    [50, 504],
+    [582, 504],
+  ];
 
   const webcamRef = useRef<any>(null);
 
@@ -17,7 +25,26 @@ function App() {
 
   useEffect(() => {
     if (picNums > 3) {
-      console.log(pics);
+      setCanvasState(() => "");
+      {
+        pics.length !== 0 &&
+          pics.map((pic, index) => {
+            const draw = canvas.getContext("2d");
+
+            const img = new Image();
+            img.src = pic;
+            img.onload = function () {
+              draw!.drawImage(
+                img,
+                canvasLocation[index][0],
+                canvasLocation[index][1],
+                512,
+                384
+              );
+            };
+          });
+      }
+
       // canvas.toBlob((blob: any) => {
       //   //캔버스의 이미지를 파일 객체로 만드는 과정
       //   let file = new File([blob], "fileName.jpg", { type: "image/jpeg" });
@@ -32,10 +59,19 @@ function App() {
     }
   }, [picNums]);
 
+  const downloadPic = () => {
+    const image = canvas.toDataURL(); // 이미지 저장하는 코드
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "혜규네컷";
+    link.click();
+  };
+
   function GoToCamera() {
     // 다시 촬영
     setPicNums(() => 0);
     setPics(() => []);
+    setCanvasState(() => "none");
   }
 
   return (
@@ -48,6 +84,13 @@ function App() {
       }}
     >
       <>
+        <canvas
+          id="canvas"
+          width="1144px"
+          height="938px"
+          style={{ display: canvasState }}
+        ></canvas>
+
         {picNums <= 3 ? (
           <>
             <Webcam
@@ -91,35 +134,6 @@ function App() {
         ) : (
           <>
             <div
-              style={{
-                width: "1920px",
-                height: "1080px",
-                display: "grid",
-                gridTemplateColumns: "512px 512px",
-                gridRow: "auto auto",
-                gridColumnGap: "20px",
-                gridRowGap: "20px",
-                margin: "20px auto",
-                backgroundColor: "#fff",
-              }}
-            >
-              {pics.length !== 0 &&
-                pics.map((pic) => (
-                  <img
-                    src={pic}
-                    style={{
-                      padding: "20px",
-                      display: "flex",
-                      alignItems: "center",
-                      justifyContent: "center",
-                      width: "512px",
-                      height: "384px",
-                    }}
-                  />
-                ))}
-            </div>
-
-            <div
               onClick={GoToCamera}
               style={{
                 display: "flex",
@@ -129,15 +143,31 @@ function App() {
                 height: "70px",
                 margin: "10px",
                 borderRadius: "10px",
-                position: "absolute",
-                zIndex: "101",
                 bottom: "5%",
                 left: "46%",
                 cursor: "pointer",
                 backgroundColor: "white",
               }}
             >
-              <p>다시 촬영</p>
+              <p>다시 찍기</p>
+            </div>
+            <div
+              onClick={downloadPic}
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                width: "70px",
+                height: "70px",
+                margin: "10px",
+                borderRadius: "10px",
+                bottom: "5%",
+                left: "46%",
+                cursor: "pointer",
+                backgroundColor: "white",
+              }}
+            >
+              <p> 다운로드 </p>
             </div>
           </>
         )}
